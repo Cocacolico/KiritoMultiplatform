29b0f53bbf8a1a9f690147399c703e5290ed9072
yak112 <yak112ster@gmail.com>
2024-06-07 22:09:21 +0200

    Modificaciones en la barra de navegación

--- composeApp/src/commonMain/kotlin/es/kirito/kirito/menu/domain/BottomNavigationItems.kt
+++ composeApp/src/commonMain/kotlin/es/kirito/kirito/menu/domain/BottomNavigationItems.kt
@@ -5,21 +5,28 @@ import androidx.compose.material.icons.filled.CalendarMonth
 import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
 import kirito.composeapp.generated.resources.Kirito_menu_menu_tittle
 import kirito.composeapp.generated.resources.Res
 import kirito.composeapp.generated.resources.buscar
+import kirito.composeapp.generated.resources.ic_japo_v3
 import kirito.composeapp.generated.resources.kirito_hoy_menu_tittle
 import kirito.composeapp.generated.resources.mensual
+import org.jetbrains.compose.resources.DrawableResource
 import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.vectorResource
+
 
 sealed class BottomNavigationItems(
     val route: String,
-    val icon: ImageVector,
+    val icon: ImageVector? = null,
+    val drawable: DrawableResource? = null,
     val label: StringResource
 ) {
-    data object Hoy : BottomNavigationItems("vistaHoy", Icons.Default.CalendarToday, Res.string.kirito_hoy_menu_tittle) //Image(painterResource(Res.drawable.ic_japo_v3),null)
-    data object Mensual : BottomNavigationItems("vistaMensual", Icons.Default.CalendarMonth, Res.string.mensual)
-    data object Buscar : BottomNavigationItems("buscador", Icons.Default.Search, Res.string.buscar)
-    data object Mas : BottomNavigationItems("menuUsuario", Icons.Default.Menu, Res.string.Kirito_menu_menu_tittle)
+    //Icons.Default.CalendarToday
+    data object Hoy : BottomNavigationItems(route = "vistaHoy", drawable = Res.drawable.ic_japo_v3, label = Res.string.kirito_hoy_menu_tittle)
+    data object Mensual : BottomNavigationItems(route = "vistaMensual", icon = Icons.Default.CalendarMonth, label = Res.string.mensual)
+    data object Buscar : BottomNavigationItems(route = "buscador", icon = Icons.Default.Search, label = Res.string.buscar)
+    data object Mas : BottomNavigationItems(route = "menuUsuario", icon = Icons.Default.Menu, label = Res.string.Kirito_menu_menu_tittle)
 }
--- composeApp/src/commonMain/kotlin/es/kirito/kirito/menu/presentation/BottomBar.kt
+++ composeApp/src/commonMain/kotlin/es/kirito/kirito/menu/presentation/BottomBar.kt
@@ -1,17 +1,22 @@
 package es.kirito.kirito.menu.presentation
 
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.*
 import es.kirito.kirito.menu.domain.BottomNavigationItems
 import org.jetbrains.compose.resources.stringResource
+import org.jetbrains.compose.resources.vectorResource
 
 @Composable
 fun KiritoBottomNavigation(
@@ -24,10 +29,23 @@ fun KiritoBottomNavigation(
 
         screens.forEach { screen ->
             NavigationBarItem(
-                icon = { Icon(
-                    imageVector = screen.icon,
-                    contentDescription = stringResource(resource = screen.label)
-                )  },
+                icon = {
+                    // Optimizar en el futuro
+                    if(screen.icon != null)
+                        Icon(
+                        imageVector = screen.icon,
+                        contentDescription = stringResource(resource = screen.label),
+                            modifier = Modifier
+                                .size(36.dp)
+                    )
+                    if(screen.drawable != null)
+                        Icon(
+                            imageVector = vectorResource(screen.drawable),
+                            contentDescription = stringResource(resource = screen.label),
+                            modifier = Modifier
+                                .size(36.dp)
+                        )
+                       },
                 label = { Text(stringResource(resource = screen.label)) },
                 selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
                 onClick = {
